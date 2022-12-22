25a209264bc357b1a8215137a1069b3c253f2061
Mr-Raja12 <rk9576899082@gmail.com>
2022-12-22 07:37:32 +0530

    Added : User need to Enter a Valid Last Name

--- src/com/bl/userregistration/UserRegistration.java
+++ src/com/bl/userregistration/UserRegistration.java
@@ -1,4 +1,5 @@
-//UC - 1 is to As a User need to enter a valid First Name.
+//UC - 1 is to As a User need to enter a valid First Name. (First name starts with Cap and has minimum 3 characters)
+//UC - 2 is to As a User need to enter a valid Last Name. (Last name starts with Cap and has minimum 3 characters)
 
 package com.bl.userregistration;
 
@@ -10,28 +11,35 @@ import java.util.regex.Pattern;
  * @author Raja
  *
  */
+
 public class UserRegistration {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the User's First Name");
-		String firstName = scanner.next();
-		if (validName(firstName)) {
-			System.out.println("Given first name : " + firstName + " is valid");
-		} else {
-			System.out.println("Given first name : " + firstName + " is not valid");
-		}
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the User's First Name");
+        String firstName = scanner.next();
+        System.out.println("Enter the User's Last Name");
+        String lastName = scanner.next();
+        if (validName(firstName) && validName(lastName))
+        {
+            System.out.println("Given first name : " + firstName + " & last name : " + lastName + " is valid");
+        }
+        else
+        {
+            System.out.println("Given first name : " + firstName + " & last name : " + lastName + " is not valid");
+        }
+    }
 
-	// Method to check user name
-	public static boolean validName(String name) {
-		boolean flag = false;
-		String regex = "[A-Z][a-z]{3,}"; // {3,} means above 3 char firstname
-		Pattern pattern = Pattern.compile(regex);
-		for (int i = 0; i < name.length(); i++) {
-			Matcher matcher = pattern.matcher(name);
-			flag = matcher.matches();
-		}
-		return flag;
-	}
+    // Method to check username
+    public static boolean validName(String name) {
+        boolean flag = false;
+        String regex="[A-Z][a-z]{3,}";      // {3,} means above 3 char firstname
+        Pattern pattern=Pattern.compile(regex);
+        for(int i = 0; i < name.length(); i++)
+        {
+            Matcher matcher=pattern.matcher(name);
+            flag = matcher.matches();
+        }
+        return flag;
+    }
 }
